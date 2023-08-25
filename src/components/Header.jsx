87ad934ffd49664edee5dd5213008c3a29e11ca2
cont1e <n0991330941@gmail.com>
2023-08-26 00:09:46 +0300

    creating new components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import classes from './Header.module.css';
+
 const Header = () => {
     return (
-       <header className='header'>
+       <header className={classes.header}>
         <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/C.P._Company_logo.svg/1024px-C.P._Company_logo.svg.png'/>
        </header>
     )
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,22 +1,34 @@
+import classes from './Profile.module.css';
+
 const Profile = () => {
     return (
-        <div className='content'>
+       <div className={classes.content}>
         <div>
-          <img className='maincontent-head' src='https://www.freewebheaders.com/wp-content/gallery/beautiful-landscape/cache/pink-purple-sky-and-spring-nature-landscape-header.jpg-nggid044745-ngg0dyn-1280x720x100-00f0w010c010r110f110r010t010.jpg'/>
+          <img className={classes.header} src='https://www.freewebheaders.com/wp-content/gallery/beautiful-landscape/cache/pink-purple-sky-and-spring-nature-landscape-header.jpg-nggid044745-ngg0dyn-1280x720x100-00f0w010c010r110f110r010t010.jpg'/>
         </div>
 
-        <div>
-          <img className='maincontent-photo' src='https://thumbs.dreamstime.com/b/two-hummingbird-bird-pink-flower-hummingbirds-fiery-throated-hummingbird-flying-next-to-beautiful-bloom-flower-savegre-co-95610310.jpg'/>
+        <div className={classes.avatar}>
+            <img src='https://thumbs.dreamstime.com/b/two-hummingbird-bird-pink-flower-hummingbirds-fiery-throated-hummingbird-flying-next-to-beautiful-bloom-flower-savegre-co-95610310.jpg'/>
+        </div>
+        <div className={classes.descr}>
+            <h1 className={classes.descr_name}>Nazarii Bezkorovainyi</h1>
+            <p className={classes.descr_nick}><i>@nazarii</i></p>
+            <hr />
+            <div className={classes.descr_about}>
+               <p>18yo</p>
+               <p>Student IASA</p>
+            </div>
+            
         </div>
 
-        <div>
-          My post
+        <div className={classes.posts}>
+          <h2>My post</h2>
           <div>
             New post
           </div>
           
           <div>
-            <div>
+            <div className={classes.item}>
               Post 1
             </div>
 
