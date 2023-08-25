73df3f27c5962c6f8638f5cf675dfb4d26beda0d
cont1e <n0991330941@gmail.com>
2023-08-25 15:53:22 +0300

    creating

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+       <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/C.P._Company_logo.svg/1024px-C.P._Company_logo.svg.png'/>
+       </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,33 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+        <div>
+          <img className='maincontent-head' src='https://www.freewebheaders.com/wp-content/gallery/beautiful-landscape/cache/pink-purple-sky-and-spring-nature-landscape-header.jpg-nggid044745-ngg0dyn-1280x720x100-00f0w010c010r110f110r010t010.jpg'/>
+        </div>
+
+        <div>
+          <img className='maincontent-photo' src='https://thumbs.dreamstime.com/b/two-hummingbird-bird-pink-flower-hummingbirds-fiery-throated-hummingbird-flying-next-to-beautiful-bloom-flower-savegre-co-95610310.jpg'/>
+        </div>
+
+        <div>
+          My post
+          <div>
+            New post
+          </div>
+          
+          <div>
+            <div>
+              Post 1
+            </div>
+
+            <div>
+              Post 2
+            </div>
+          </div>
+        </div>
+
+       </div>
+    )
+}
+
+export default Profile;
